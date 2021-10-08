f7b2ddb56d8738bfc1d86fe196f5e637c33fcbc5
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-10-08 10:10:02 -0700

    Add Stylist and Client models

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+
+    public virtual ICollection<Client> Clients { get; set; }
+  }
+}
